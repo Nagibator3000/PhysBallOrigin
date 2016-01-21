4254e283a8ca371201cc579354155850c1d5e9f0
Nagibator3000 <fetakonfeta@gmail.com>
2016-01-21 22:55:54 +0300

    - remake method speedAnd.... to cordInSec

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static double tsqr;
-    public static int x;
+    public static double x;
     public static double g = 9.8;
     public static double t;
-    public static double[] speeds;
+    public static double[] cord;
     public static double u;
 
     public static void main(String[] args) {
         System.out.println("Enter height m");
         Scanner in = new Scanner(System.in);
-        x = in.nextInt();
-        speeds = new double[x];
-        for (int i = 0; i < x; i++) {
-            timeAndSpeed(i);
-            speeds[i]=u;
-            System.out.println(x-i+" Height m  Speed: "+speeds[i]);
+        x = in.nextDouble();
+        tsqr = 2 * x/ g;
+        t = Math.sqrt(tsqr);
+
+
+        for (int i = 0; i < t; i++) {
+            cordInSec(i);
+            System.out.println(x);
+
         }
 
     }
 
-    public static double timeAndSpeed(int i) {
-        tsqr = 2 * i / g;
-        t = Math.sqrt(tsqr);
-        u = g * t;
-        return u;
+    public static double cordInSec(int i) {
+        x=g*(i*i)/2;
+        return x;
     }
 
 }
